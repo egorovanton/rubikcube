df5bc29401eeefa0c7b7030700a804d68f3510ba
Katsz <vetrier@gmail.com>
2017-02-04 20:57:44 +0300

    ScrambleGen added

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "ScrambleGen.h"
+
+//
+// Created by alex on 2/4/17.
+//
+int main() {
+    ScrambleGen simpleGen;
+    std::cout << simpleGen.getNewScramble().join(", ").toStdString();
+}
