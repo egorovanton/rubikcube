ad8b2581f72ab685184075a718a3660cc154a6db
egorov anton <yegorov.ant@gmail.com>
2017-02-08 19:43:16 +0400

    new functions for reading cube

--- rubikcubetwister/main.cpp
+++ rubikcubetwister/main.cpp
@@ -4,6 +4,7 @@
 #include "cube.h"
 #include "utils.h"
 #include <QDebug>
+#include <random>
 
 using namespace std;
 
@@ -19,7 +20,7 @@ int main(int argc, char *argv[])
     cout << "initial cube:" << endl << endl;
     cout << cube.print().toStdString() << endl;
 
-    QVector<QString> directions = {"F", "B2", "L\'"};
+    QVector<QString> directions = {"F'", "D", "F'", "D'", "L", "F2", "D2", "L2", "F2", "L", "B2", "R", "L", "U'", "L2", "F", "D'", "F2", "D", "B2", "U'", "L'", "U2", "B", "D"};
 
 
     for (auto str: directions) {
@@ -30,6 +31,37 @@ int main(int argc, char *argv[])
     }
 
     cout << cube.print().toStdString() << endl;
+
+    default_random_engine gen;
+    uniform_int_distribution<int> randomPlane(0, 5);
+    uniform_int_distribution<int> randomInt(0, 3);
+    for (int i = 0; i < 10; ++i) {
+        PlaneType plane = (PlaneType) randomPlane(gen);
+        QVector<PlaneType> neighbours = Direction(plane, CLOCKWISE).getNeighbours();
+        int neighbourIndex = randomInt(gen);
+        PlaneType plane1 = neighbours[neighbourIndex];
+        PlaneType plane2 = neighbours[(neighbourIndex + 1) % 4];
+
+        cout << "plane A = " << plane << endl;
+        cout << "plane B = " << plane1 << endl;
+        cout << "plane C = " << plane2 << endl;
+
+        int x1, x2, x3;
+        tuple<PlaneType, PlaneType> p;
+        tuple<PlaneType, PlaneType, PlaneType> t;
+
+        p = cube.getCubie(plane, plane1);
+        cout << "between A and B is " << get<0>(p) << " " << get<1>(p) << endl;
+        p = cube.getCubie(plane1, plane2);
+        cout << "between B and C is " << get<0>(p) << " " << get<1>(p) << endl;
+        p = cube.getCubie(plane2, plane);
+        cout << "between C and A is " << get<0>(p) << " " << get<1>(p) << endl;
+        t = cube.getCubie(plane, plane1, plane2);
+        cout << "between A, B and C is " << get<0>(t) << " " << get<1>(t) <<  " " << get<2>(t) << endl;
+        cout << endl << endl;
+    }
+
+
 
     return a.exec();
 }
