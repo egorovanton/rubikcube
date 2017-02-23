4baf9b61085ec464f694866516d1f092e49fcfa9
Katsz <vetrier@gmail.com>
2017-02-23 18:58:49 +0300

    Waiting Cube::fitsMask fixing

--- solver/solver.h
+++ solver/solver.h
@@ -4,48 +4,75 @@
 
 #ifndef RUBIKCUBE_SOLVER_H
 #define RUBIKCUBE_SOLVER_H
+#define S(t) ((QString) t).split(" ")
+#define F FRONT
+#define U UP
+#define B BACK
+#define L LEFT
+#define R RIGHT
 
 
 #include "../twister/cube.h"
+#include "../twister/mask.h"
 
 class Solver {
 public:
     Solver(Cube c);
+
     QStringList solve();
 
-private:
-    Cube cube;
-public:
+    QStringList solveF2L();
+
+    QStringList getPreparedResultF2L();
+
     const Cube &getCube() const;
 
     QStringList getPreparedResult();
+
 private:
+    Cube cube;
     QStringList result;
     const PlaneType sides[4] = {FRONT, LEFT, BACK, RIGHT};
 
     void cross();
+
     bool upPlane(PlaneType currentSide);
+
     bool downPlane(PlaneType currentSide);
+
     bool midPlane(PlaneType currentSide);
+
     void dropTopLeft(PlaneType cur);
+
     void dropTopRight(PlaneType cur);
 
+    //Fridric
+    bool F2L();
+
     void bottomCorners();
+
     bool alignBottomCorner(PlaneType f);
+
     bool downCornerMatch(std::tuple<PlaneType, PlaneType, PlaneType> t, PlaneType l);
 
     void middleLine();
+
     bool nextMiddleCubie();
 
     void upperOrientation();
+
     void upperOrder();
+
     void upperCross();
 
     void rotate(QString s);
+
     void rotate(QStringList q) {
-        for(auto s:q) rotate(s);
+        for (auto s:q) rotate(s);
     }
+
     void makeSeparator();
+
     QString topTwist(PlaneType from, PlaneType to);
 
     void flipUpperCorners();
@@ -55,6 +82,58 @@ private:
     bool uppCornerMatch(std::tuple<PlaneType, PlaneType, PlaneType> t, PlaneType l);
 
     void upperCorners();
+
+    const QVector<QVector<Duo>> middleD = {
+            {{F, R}, {R, F}, {U, R}, {L, U}, {U, B}, {U, L}, {B, U}, {R, U}, {F, U}, {U, F}},
+            {{F, R}, {R, F}, {F, U}, {U, B}, {L, U}, {B, U}, {U, L}, {U, F}, {U, R}, {R, U}},
+            {{F, R}, {R, F}, {F, U}, {U, F}, {R, U}, {U, F}},
+            {{F, U}},
+            {{U, R}},
+            {{F, U}, {U, R}},
+            {{F, R}, {R, F}, {F, U}, {U, R}},
+            {{F, R}, {R, F}, {F, U}, {U, R}},
+            {{F, U}, {U, R}, {R, F}}
+    };
+    const QVector<QVector<QStringList>> rot = {
+            {S("U' R U' R' U2 R U' R'"),  S("U2 R U R' y U' L' U L"), S("U R U' R'"),
+             S("F' U' F"), S("U' R U R' U2 R U' R'"),
+             S("U' R U2 R' U2 R U' R'"),  S("F R U R' U' F' R U' R'"), S("y' R U2 R2 U' R2 U' R'"),
+             S("y' U R' U R U' R' U' R"),  S("R U R' U2 R U' R' U R U' R'")},
+
+             {S("U' R U2' R' U R U R'"), S("U F' U' F U' R U R'"),
+             S("y U' L' U L"),    S("R U R'"),  S("y' U R' U' R U R' U2 R"),
+             S("y' U R' U2 R U R' U2 R"), S("U' R U R' U R U R'"),
+             S("R U' R' U R U' R' U2 R U' R'"), S("U' R U' R' U R U R'"), S("R U' R' y' U2 R' U' R")},
+
+             {S("U R U' R' U R U' R' U R U' R'"), S("R U' R' U y' R' U R"),
+              S("y' R' U2 R U R' U' R"), S("R U2 R' U' R U R'"),
+              S("y U' L' U L U L' U L U' L' U L"), S("R U' R' U R U2 R' U R U R'")},
+
+             {S("y' R' U2 R U' R' U R")},
+
+             {S("R U2 R' U R U' R'")},
+
+             {S("R U R' F' U' F"),  S("R U R' U R U' R'")},
+
+             {S("R U' R' U' R U R' U2 R U' R'"),  S("R F U R U' R' F' U' R'"),
+              S("y L' U' L U L' U' L"),  S("R U' R' U R U' R'")},
+
+             {S("R U2 R U R' U R U2 R2'"), S("y F R U2 R' F' L' U' L"),
+              S("y L' U L U' L' U L"),   S("R U R' U' R U R'")},
+
+             {S("U R U' R' F R' F' R"),   S("R' F' R U R U' R' F"),  S("R U' R' U y' R' U2 R U2' R' U R")}
+            };
+    const QVector<Triple> corners = {
+            {U,    F, R},
+            {F,    R, U},
+            {R,    U, F},
+            {B,    U, R},
+            {F,    U, L},
+            {F,    U, B},
+            {R,    F, DOWN},
+            {DOWN, R, F},
+            {F, DOWN, R}
+    };
 };
 
 
--- twister/direction.cpp
+++ twister/direction.cpp
@@ -4,10 +4,10 @@
 
 
 const QSet<QString> Direction::validDirecions = {
-    "L",  "R",  "U",  "D",  "F",  "B",
-    "L'", "R'", "U'", "D'", "F'", "B'",
-    "L2", "R2", "U2", "D2", "F2", "B2",
-    "Y"
+        "L", "R", "U", "D", "F", "B",
+        "L'", "R'", "U'", "D'", "F'", "B'",
+        "L2", "R2", "U2", "D2", "F2", "B2",
+        "Y"
 };
 
 PlaneType Direction::getPlane() const {
@@ -20,54 +20,55 @@ Direction::Direction(QString str) {
     //    }
 
     switch (str[0].toUpper().toLatin1()) {
-    case 'F':
-        plane = FRONT;
-        break;
-    case 'R':
-        plane = RIGHT;
-        break;
-    case 'U':
-        plane = UP;
-        break;
-    case 'B':
-        plane = BACK;
-        break;
-    case 'L':
-        plane = LEFT;
-        break;
-    case 'D':
-        plane = DOWN;
-        break;
-    case 'M':
-        plane = MIDDLE;
-        break;
-    case 'E':
-        plane = EQUATOR;
-        break;
-    case 'S':
-        plane = STANDING;
-        break;
-    case 'Y':
-        plane = WHOLE_Y;
-        break;
+        case 'F':
+            plane = FRONT;
+            break;
+        case 'R':
+            plane = RIGHT;
+            break;
+        case 'U':
+            plane = UP;
+            break;
+        case 'B':
+            plane = BACK;
+            break;
+        case 'L':
+            plane = LEFT;
+            break;
+        case 'D':
+            plane = DOWN;
+            break;
+        case 'M':
+            plane = MIDDLE;
+            break;
+        case 'E':
+            plane = EQUATOR;
+            break;
+        case 'S':
+            plane = STANDING;
+            break;
+        case 'y':
+        case 'Y':
+            plane = WHOLE_Y;
+            break;
 
-    default:
-        plane = STUB;
-        break;
+        default:
+            plane = STUB;
+            break;
     }
 
     if (str.length() == 1) {
         rotation = CLOCKWISE;
     } else {
         switch (str[1].toLatin1()) {
-        case '\'':
-            rotation = COUNTER_CLOCKWISE;
-            break;
-        case '2':
-            rotation = HALF_TURN;
-            break;
-        default:
-            break;
+            case '\'':
+                rotation = COUNTER_CLOCKWISE;
+                break;
+            case '2':
+                rotation = HALF_TURN;
+                break;
+            default:
+                break;
         }
     }
 
@@ -100,44 +101,44 @@ Rotation Direction::getRotation() const {
 QString Direction::merge(Direction d) {
     QString result = planeToString(plane);
     switch (rotation) {
-    case COUNTER_CLOCKWISE:
-        switch (d.rotation) {
         case COUNTER_CLOCKWISE:
-            result.append("2");
-            break;
-        case HALF_TURN:
+            switch (d.rotation) {
+                case COUNTER_CLOCKWISE:
+                    result.append("2");
+                    break;
+                case HALF_TURN:
+                    break;
+                case CLOCKWISE:
+                    result = "";
+                    break;
+            }
             break;
         case CLOCKWISE:
-            result = "";
-            break;
-        }
-        break;
-    case CLOCKWISE:
-        switch (d.rotation) {
-        case COUNTER_CLOCKWISE:
-            result = "";
-            break;
-        case HALF_TURN:
-            result.append("\'");
+            switch (d.rotation) {
+                case COUNTER_CLOCKWISE:
+                    result = "";
+                    break;
+                case HALF_TURN:
+                    result.append("\'");
 
-            break;
-        case CLOCKWISE:
-            result.append("2");
-            break;
-        }
-        break;
-    case HALF_TURN:
-        switch (d.rotation) {
-        case COUNTER_CLOCKWISE:
+                    break;
+                case CLOCKWISE:
+                    result.append("2");
+                    break;
+            }
             break;
         case HALF_TURN:
-            result = "";
+            switch (d.rotation) {
+                case COUNTER_CLOCKWISE:
+                    break;
+                case HALF_TURN:
+                    result = "";
+                    break;
+                case CLOCKWISE:
+                    result.append("\'");
+                    break;
+            }
             break;
-        case CLOCKWISE:
-            result.append("\'");
-            break;
-        }
-        break;
     }
     return result;
 }
